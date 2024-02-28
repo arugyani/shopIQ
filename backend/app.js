5dd2fa7c7e5645d93406f20b4500371d14262d59
divyamk <divyam.khatri@yahoo.com>
2024-02-27 22:56:02 -0600

    Added Dockerfile and dockerignore

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+/**
+ * Filename: app.js
+ * Project: ShopIQ
+ * Author: Divyam Khatri, Junseok Jang
+ * Created: 02/27/2024
+ * Description: Entry point for the Express application.
+ */
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
