40690db15b6e6904bf4b6ef30902b535cc0f8c6a
Aru Gyani <gyaniaru@gmail.com>
2024-03-22 01:15:52 -0500

    ci: update vite base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/shopIQ",
+  base: "/shopIQ/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
