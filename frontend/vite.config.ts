4b39d2b48673c4be099827812d69bd934df4b1d3
Aru Gyani <gyaniaru@gmail.com>
2024-03-22 17:17:04 -0500

    fix: deletes unused base path config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://arugyani.github.io/shopIQ/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
