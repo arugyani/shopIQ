53e7905391e381231d7095dc6362bf03b6d03ea7
Aru Gyani <gyaniaru@gmail.com>
2024-03-23 02:21:00 -0500

    dev: adds cors for dev interaction w/ frontend

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,15 @@
  * Description: Entry point for the Express application.
  */
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
 
 const searchRouter = require("./src/routes/searchRoutes");
 const recommendationRoute = require("./src/routes/recommendationRoute");
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
