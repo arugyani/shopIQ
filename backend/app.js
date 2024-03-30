e714458ee8fec1090c7d655ba9b1451813e3bd9e
divyamk <divyam.khatri@yahoo.com>
2024-03-29 21:16:32 -0500

    feat: added integration for Gemini API

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,18 @@
  * Created: 02/27/2024
  * Description: Entry point for the Express application.
  */
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
+const fetch = require('cross-fetch');
+global.fetch = fetch;
 
 const app = express();
 const port = 3000;
 
 const searchRouter = require("./src/routes/searchRoutes");
 const recommendationRoute = require("./src/routes/recommendationRoute");
+const searchService = require("./src/services/searchService");
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -25,3 +29,8 @@ app.use("/recommendations", recommendationRoute);
 app.listen(port, () => {
   console.log(`shopIQ API listening on port ${port} 😎`);
 });
+
+(async () => {
+  console.log(await searchService.getLLMResponse("What is the best LLM?"));
+})();
+
--- backend/src/services/searchService.js
+++ backend/src/services/searchService.js
@@ -1,9 +1,21 @@
 const productQuestions = require("../dummy/product-questions.json");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const getLLMResponse = () => {
+const getDummyLLMResponse = () => {
   return productQuestions.response;
 };
 
+const getLLMResponse = async (prompt) => {
+
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+
+}
 module.exports = {
   getLLMResponse,
+  getDummyLLMResponse
 };
