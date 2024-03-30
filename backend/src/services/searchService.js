c1c20fb1a547b8940ae63b0d0171e2dc68068f23
divyamk <divyam.khatri@yahoo.com>
2024-03-29 22:51:50 -0500

    fix: added comment for finding api key

--- backend/src/services/searchService.js
+++ backend/src/services/searchService.js
@@ -7,7 +7,11 @@ const getDummyLLMResponse = () => {
 };
 
 const getLLMResponse = async (prompt) => {
-
+  /*
+    API key is stored in .env file.
+    .env file can be found in discord/backend-notes
+    env file should be placed in backend folder
+  */
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
   const result = await model.generateContent(prompt);
   const response = await result.response;
