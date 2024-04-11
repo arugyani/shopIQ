8c3e9b1e2400aa8cda286cd70aceb943b049d7d9
divyamk <divyam.khatri@yahoo.com>
2024-04-11 16:13:28 -0500

    feat: added InfoBlurb component

--- frontend/src/components/custom/InfoBlurb.tsx
+++ frontend/src/components/custom/InfoBlurb.tsx
@@ -2,15 +2,24 @@
 // import { useAppDispatch, useAppSelector } from "@/app/hooks";
 
 import { Info } from "lucide-react";
+import { FC } from "react";
 // import { Input } from "../ui/input";
 // import { LoadingSpinner } from "../ui/LoadingSpinner";
 
+interface InfoBlurbProps{
+  info: string;
+}
 
-
-const InfoBlurb = () => {
+const InfoBlurb: FC<InfoBlurbProps> = ({ info }) => {
   return (
-      <Info/>
+    <div className="relative group ">
+      <Info className="cursor-pointer text-gray-500" style={{ fontSize: '24px' }} />
+      <div className="absolute bottom-4 left-10 opacity-0 z-10 text-sm line-clamp-2 bg-white p-2 border rounded shadow-lg transition-opacity group-hover:opacity-100">
+        {info}
+      </div>
+    </div>
   );
 };
 
 export default InfoBlurb;
+
