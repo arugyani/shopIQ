0fe43e2af4820bcc59da056ce95f273ceeff6fb5
divyamk <divyam.khatri@yahoo.com>
2024-04-14 18:40:45 -0500

    fix: added files to use puppeteer on deployment

--- backend/src/services/scrape_filers.js
+++ backend/src/services/scrape_filers.js
@@ -3,8 +3,8 @@ const puppeteer = require("puppeteer");
 async function scrapeFilter(search) {
   const browser = await puppeteer.launch({
     headless: true,
-    devtools: true,
-    args: ["--lang=en-US"],
+    devtools: false,
+    args: ['--no-sandbox'],
   });
   const page = await browser.newPage();
   await page.goto("https://shopping.google.com/", {
